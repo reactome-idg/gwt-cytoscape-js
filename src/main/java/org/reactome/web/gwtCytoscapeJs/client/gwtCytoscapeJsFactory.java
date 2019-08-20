c7e4c3efaead9060fe028cc719ff3301efb01cec
Tim Brunson <tjbrunson@gmail.com>
2019-08-20 10:30:03 -0700

    EventBus setup

---
+++ src/main/java/org/reactome/web/gwtCytoscapeJs/client/gwtCytoscapeJsFactory.java
@@ -0,0 +1,8 @@
+package org.reactome.web.gwtCytoscapeJs.client;
+
+public class gwtCytoscapeJsFactory {
+	
+    public static boolean CONSOLE_VERBOSE = false;
+    public static boolean EVENT_BUS_VERBOSE = false;
+	
+}
---
+++ src/main/java/org/reactome/web/gwtCytoscapeJs/util/CytoscapeEventBus.java
@@ -0,0 +1,37 @@
+package org.reactome.web.gwtCytoscapeJs.util;
+
+import java.util.Date;
+
+import org.reactome.web.gwtCytoscapeJs.client.gwtCytoscapeJsFactory;
+import org.reactome.web.gwtCytoscapeJs.util.Console;
+
+import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.event.shared.SimpleEventBus;
+import com.google.gwt.i18n.client.DateTimeFormat;
+
+/**
+ * @author brunsont
+ */
+public class CytoscapeEventBus extends SimpleEventBus {
+	
+    private DateTimeFormat fmt = DateTimeFormat.getFormat("HH:mm:ss");
+
+	
+	@Override
+    public void fireEvent(GwtEvent<?> event) {
+        String msg = "Please do not use fireEvent. Use fireEventFromSource instead.";
+        throw new RuntimeException(msg);
+    }
+
+    @Override
+    public void fireEventFromSource(GwtEvent<?> event, Object source) {
+        if(gwtCytoscapeJsFactory.EVENT_BUS_VERBOSE) {
+            Console.info(
+                    this.fmt.format(new Date()) + " " +
+                            source.getClass().getSimpleName() + " >> " +
+                            event
+            );
+        }
+        super.fireEventFromSource(event, source);
+    }
+}
